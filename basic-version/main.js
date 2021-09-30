91edf7e4e01dad3cd0ad27e22af46ba14256054e
Jethet <henriette.hettinga@protonmail.com>
2021-09-30 17:01:15 +0200

    Clean up code

--- basic-version/main.js
+++ basic-version/main.js
@@ -4,32 +4,39 @@ let password = document.querySelector("#password");
 let form = document.querySelector("#form");
 let errorMessage = document.querySelector(".error");
 let successMessage = document.querySelector(".success");
-let successIcon = document.querySelector(".success-icon");
-let failureIcon = document.querySelector(".failure-icon");
 let button = document.querySelector("#button");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("clicked");
+  console.log(form.elements[0]);
+  
 
   if (username.value === "") {
     console.log("error username");
     errorMessage.innerText = "Input field cannot be blank";
     username.style.border = "2px solid red";
-  } else if (email.value === "") {
+  }
+  if (email.value === "") {
     console.log("error email");
     errorMessage.innerText = "Input field cannot be blank";
     email.style.border = "2px solid red";
-  } else if (password.value === "") {
+  }
+  if (password.value === "") {
     console.log("error password");
     errorMessage.innerText = "Input field cannot be blank";
     password.style.border = "2px solid red";
-  } else {
-    errorMessage.innerText = "";
-    successMessage.innerText = "All input fields filled in correctly";
-    username.style.border = "2px solid green"
-    email.style.border = "2px solid green"
-    password.style.border = "2px solid green"
   }
+  errorMessage.innerText = "";
+  successMessage.innerText = "All input fields filled in correctly";
+  username.style.border = "2px solid green";
+  email.style.border = "2px solid green";
+  password.style.border = "2px solid green";
+  resetForm();
 });
 
+const resetForm = () => {
+  username.value = "";
+  email.value = "";
+  password.value = "";
+};
