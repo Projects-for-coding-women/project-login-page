d10108d71b2dcff0ddb9cc7d2de18082a19f8991
Jethet <henriette.hettinga@protonmail.com>
2021-09-30 17:41:32 +0200

    Add form reset

--- basic-version/main.js
+++ basic-version/main.js
@@ -10,33 +10,33 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("clicked");
   console.log(form.elements[0]);
-  
 
   if (username.value === "") {
     console.log("error username");
     errorMessage.innerText = "Input field cannot be blank";
     username.style.border = "2px solid red";
-  }
-  if (email.value === "") {
+  } else if (email.value === "") {
     console.log("error email");
     errorMessage.innerText = "Input field cannot be blank";
     email.style.border = "2px solid red";
-  }
-  if (password.value === "") {
+  } else if (password.value === "") {
     console.log("error password");
     errorMessage.innerText = "Input field cannot be blank";
     password.style.border = "2px solid red";
+  } else {
+    errorMessage.innerText = "";
+    successMessage.innerText = "All input fields filled in correctly";
+    username.style.border = "2px solid green";
+    email.style.border = "2px solid green";
+    password.style.border = "2px solid green";
   }
-  errorMessage.innerText = "";
-  successMessage.innerText = "All input fields filled in correctly";
-  username.style.border = "2px solid green";
-  email.style.border = "2px solid green";
-  password.style.border = "2px solid green";
+
   resetForm();
 });
 
 const resetForm = () => {
-  username.value = "";
-  email.value = "";
-  password.value = "";
+  document.getElementById("form").reset();
+  username.style.border = "";
+  email.style.border = "";
+  password.style.border = "";
 };
