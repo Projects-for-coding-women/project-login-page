baa6dc9b3dd4a0af68b9cb51b073dfd80e3a7e39
Jethet <henriette.hettinga@protonmail.com>
2021-11-22 15:57:14 +0100

    Change error message position, no solution

--- advanced-version/main.js
+++ advanced-version/main.js
@@ -15,63 +15,31 @@ form.addEventListener("submit", (e) => {
     errorMessage.innerText = "Username cannot be blank";
     failureIcon.style.opacity = "1";
     successIcon.style.opacity = "0";
-  }
-  if (email.value === "") {
+  
+  } else if (email.value === "") {
     email.style.border = "2px solid red";
     errorMessage.innerText = "Email cannot be blank";
     failureIcon.style.opacity = "1";
     successIcon.style.opacity = "0";
-  }
-  if (password.value === "") {
+
+  } else if (password.value === "") {
     password.style.border = "2px solid red";
     errorMessage.innerText = "Password cannot be blank";
     failureIcon.style.opacity = "1";
     successIcon.style.opacity = "0";
+
+  } else {
+    errorMessage.innerText = "";
+    successMessage.innerText = "All input fields filled in correctly";
+    username.style.border = "2px solid green";
+    email.style.border = "2px solid green";
+    password.style.border = "2px solid green";
+    failureIcon.style.opacity = "0";
+    successIcon.style.opacity = "1";
+    resetForm();
   }
-  errorMessage.innerText = "";
-  username.style.border = "2px solid green";
-  email.style.border = "2px solid green";
-  password.style.border = "2px solid green";
-  failureIcon.style.opacity = "0";
-  successIcon.style.opacity = "1";
-  successMessage.innerText = "All input fields filled in correctly";
-  resetForm();
 });
 
-//   if (username.value === "") {
-//     username.style.border = "2px solid red";
-//     errorMessage.innerText = "Username cannot be blank";
-//     failureIcon.style.opacity = "1";
-//     successIcon.style.opacity = "0";
-//   } else {
-//     username.style.border = "2px solid green";
-//     successIcon.style.opacity = "1";
-//     failureIcon.style.opacity = "0";
-//   }
-
-//   if (email.value === "") {
-//     email.style.border = "2px solid red";
-//     errorMessage.innerText = "Email cannot be blank";
-//     failureIcon.style.opacity = "1";
-//     successIcon.style.opacity = "0";
-//   } else {
-//     username.style.border = "2px solid green";
-//     successIcon.style.opacity = "1";
-//     failureIcon.style.opacity = "0";
-//   }
-
-//   if (password.value === "") {
-//     password.style.border = "2px solid red";
-//     errorMessage.innerText = "Password cannot be blank";
-//     failureIcon.style.opacity = "1";
-//     successIcon.style.opacity = "0";
-//   } else {
-//     username.style.border = "2px solid green";
-//     successIcon.style.opacity = "1";
-//     failureIcon.style.opacity = "0";
-//     resetForm()
-//   }
-// });
 
 const resetForm = () => {
   document.getElementById("form").reset();
