224a681d2dc3e1c7f3ffa43ba3e9d8cf4d9f15b1
Jethet <henriette.hettinga@protonmail.com>
2021-11-22 15:37:13 +0100

    Add failure and success icons, functionality

--- advanced-version/main.js
+++ advanced-version/main.js
@@ -9,48 +9,42 @@ let button = document.querySelector("#button");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("clicked");
 
-  if (username.value === "") {
-    console.log("error username");
-   
+  if (username.value === "") {   
     username.style.border = "2px solid red";
     failureIcon.style.opacity = "1";
     successIcon.style.opacity = "0";
-  } else if (email.value === "") {
-    console.log("error email");
-
+  } else {
+    username.style.border = "2px solid green";
+    successIcon.style.opacity = "1";
+    failureIcon.style.opacity = "0";
+  }
+  
+  if (email.value === "") {
     email.style.border = "2px solid red";
     failureIcon.style.opacity = "1";
     successIcon.style.opacity = "0";
-  } else if (password.value === "") {
-    console.log("error password");
-
+  } else {
+    username.style.border = "2px solid green";
+    successIcon.style.opacity = "1";
+    failureIcon.style.opacity = "0";
+  }
+  
+  if (password.value === "") {
     password.style.border = "2px solid red";
     failureIcon.style.opacity = "1";
     successIcon.style.opacity = "0";
   } else {
-    errorMessage1.innerText = "";
-    errorMessage2.innerText = "";
-    errorMessage3.innerText = "";
+    username.style.border = "2px solid green";
     successIcon.style.opacity = "1";
     failureIcon.style.opacity = "0";
   }
 });
 
-// let validation = (id, serial, message) => {
-//   if (id.value === "") {
-//     errorMessage[serial].innerHTML = message
 
-//     id.style.border = "2px solid red"
-//     failureIcon[serial].style.opacity = "1"
-//     successIcon[serial].style.opacity = "0"
-//   }
-//   else {
-//     errorMessage[serial].innerHTML = ""
-
-//     id.style.border = "2px solid green"
-//     failureIcon[serial].style.opacity = "0"
-//     successIcon[serial].style.opacity = "1"
-//   }
-// }
+const resetForm = () => {
+  document.getElementById("form").reset();
+  username.style.border = "";
+  email.style.border = "";
+  password.style.border = "";
+};
