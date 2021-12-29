4a1d5483531b65d91948295f53bb51b00f7982b7
Jethet <henriette.hettinga@protonmail.com>
2021-12-29 21:51:00 +0100

    Add timeout; throws error

--- graduate-version/main.js
+++ graduate-version/main.js
@@ -1,15 +1,3 @@
-// let username = document.querySelector("#username");
-// let email = document.querySelector("#email");
-// let password = document.querySelector("#password");
-// let form = document.querySelector("#form");
-// let errorName = document.querySelector(".error-username");
-// let errorEmail = document.querySelector(".error-email");
-// let errorPassword = document.querySelector(".error-password");
-// let message = document.querySelector(".message");
-// let successIcon = document.querySelector(".success-icon");
-// let failureIcon = document.querySelector(".failure-icon");
-// let button = document.querySelector(".button");
-
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
 
@@ -20,7 +8,6 @@ let form = id("form");
 let errorMessage = classes("error");
 let successIcon = classes("success-icon");
 let failureIcon = classes("failure-icon");
-let message = classes("message")
 
 let validateFunction = (id, serial, message) => {
   if (id.value.trim() === "") {
@@ -33,14 +20,12 @@ let validateFunction = (id, serial, message) => {
     id.style.border = "2px solid green";
     failureIcon[serial].style.opacity = "0";
     successIcon[serial].style.opacity = "1";
-    message.innerHTML = "All fields filled in correctly"
   }
-  setTimeout(resetForm, 2000)
+  setTimeout(resetForm, 3000)    
 };
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   validateFunction(username, 0, "Username cannot be blank");
   validateFunction(email, 1, "Email cannot be blank");
   validateFunction(password, 2, "Password cannot be blank");
@@ -52,15 +37,6 @@ const resetForm = () => {
   username.style.border = "";
   email.style.border = "";
   password.style.border = "";
-  message.innerText = "";
   successIcon.style.opacity = "0";
   failureIcon.style.opacity = "0";
 };
-
-function validateEmail() {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(form.email.value)) {
-    return true;
-  }
-  alert("You have entered an invalid email address!");
-  return false;
-}
